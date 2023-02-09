65d3124c70df797269137eb522410461bbe0803f
Keyhan Rezvan <117317736+Keyhanrezvan@users.noreply.github.com>
2023-02-09 13:53:19 -0600

    Update dishes.controller.js

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,9 +1,5 @@
 const path = require("path");
-
-// Use the existing dishes data
 const dishes = require(path.resolve("src/data/dishes-data"));
-
-// Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
 
